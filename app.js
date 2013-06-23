eb9fcb657424dd3534b1f2a21c0401ef0009993d
mmoman <mmoman@mmoman.bsd-host.de>
2013-06-23 08:35:35 +0200

    app fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.configure(function(){
-  server.use('/set', express.static(__dirname + '/public'));
+  app.use('/set', express.static(__dirname + '/public'));
   app.use(express.static(__dirname + '/public'));
 });
 
